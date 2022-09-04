c50562ac46be483b6eed55d574be442802d609d9
Sagiv Mishaan <sagiv505@gmail.com>
2022-09-04 19:25:20 +0300

    Vercel integration

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 })
 
 // Register your application as a normal plugin.
-app.register(import("../src/app"))
+app.register(import("../server"))
 
 export default async (req, res) => {
   await app.ready()
